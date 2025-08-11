a622e621af4dfdbdf17029363b80ec1ae186ffd1
iamdrishtiii <drishti.web.dev.work@gmail.com>
2025-08-12 02:40:33 +0530

    first commit

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -146,7 +146,7 @@ const Signup = ({ setActive, setUser }) => {
           setOpenModal(false)
           navigate("/")
         }, 1000);
-        setUser(name);
+        setUser(user);
         setName(""); setEmail(""); setPassword(""); setRepeatPassword("");
       })
       .catch((error) => {
