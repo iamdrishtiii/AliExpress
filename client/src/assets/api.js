b660d950ddffecbf89e69008d8ec7c1262bd3849
iamdrishtiii <drishti.web.dev.work@gmail.com>
2026-01-10 01:58:58 +0530

    .

--- client/src/assets/api.js
+++ client/src/assets/api.js
@@ -1,12 +1,12 @@
 // export const producturl ="https://fakestoreapi.in/api/products"
 // export const categoryurl ="https://fakestoreapi.in/api/products/category"
-// export const Authurl="https://aliexpress-92fc.onrender.com"
+// export const Authurl="https://aliexpress-qmgi.onrender.com"
 
-export const producturl ="http://localhost:3000/products"
-export const categoryurl ="http://localhost:3000/categories"
-export const Authurl="http://localhost:3000.com"
+// export const producturl ="http://localhost:3000/products"
+// export const categoryurl ="http://localhost:3000/categories"
+// export const Authurl="http://localhost:3000.com"
 
-// export const producturl ="https://aliexpress-92fc.onrender.com/products"
-// export const categoryurl ="https://aliexpress-92fc.onrender.com/categories"
-// export const Authurl="https://aliexpress-92fc.onrender.com"
+export const producturl ="https://aliexpress-qmgi.onrender.com/products"
+export const categoryurl ="https://aliexpress-qmgi.onrender.com/categories"
+export const Authurl="https://aliexpress-qmgi.onrender.com"
 
